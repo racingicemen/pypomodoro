04be1903b40ebee1c8da5bb4d0e497d6a528eb93
Racing Icemen (orange) <rkkommu@ucdavis.edu>
2021-02-19 18:09:21 -0800

    Add alarm sound. Cleanup code

--- v1/pomodoro.py
+++ v1/pomodoro.py
@@ -1,32 +1,34 @@
-import sys, datetime
+import sys
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, QLCDNumber, QPushButton,QTabWidget, QVBoxLayout, QHBoxLayout
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtMultimedia
 from PomodoroStyleSheet import style_sheet
 
 MINUTES = 60 * 1000             # milliseconds in a minute
-POMODORO_TIME = 3 * MINUTES    # pomdoro duration in minutes
-SHORT_BREAK_TIME = 1 * MINUTES  # short break time
-LONG_BREAK_TIME = 2 * MINUTES  # long break time
+POMODORO_TIME = 30 * MINUTES    # pomodoro duration in minutes
+SHORT_BREAK_TIME = 6 * MINUTES  # short break time
+LONG_BREAK_TIME = 60 * MINUTES  # long break time
 
 
 class PomodoroTimer(QWidget):
     def __init__(self):
         super().__init__()
-        self.pomodoro_sound = self.initialize_sound_files('clock-ticking-5.wav')
-        self.sbreak_sound = self.initialize_sound_files('clock-ticking-1.wav')
-        self.lbreak_sound = self.initialize_sound_files('clock-ticking-2.wav')
-        self.initializeUI()
+        self.pomodoro_sound = PomodoroTimer.initialize_sound_files('clock-ticking-2.wav')
+        self.short_break_sound = PomodoroTimer.initialize_sound_files('clock-ticking-1.wav')
+        self.long_break_sound = PomodoroTimer.initialize_sound_files('clock-ticking-5.wav')
+        self.alarm_sound = PomodoroTimer.initialize_sound_files('alarm-clock-01.wav')
+        self.initialize_ui()
 
-    def initialize_sound_files(self, sound_file_name):
+    @staticmethod
+    def initialize_sound_files(sound_file_name):
         sound = QtMultimedia.QSoundEffect()
         sound.setSource(QtCore.QUrl.fromLocalFile(sound_file_name))
         sound.setVolume(1.0)
         return sound
 
-    def initializeUI(self):
+    def initialize_ui(self):
         self.setMinimumSize(500, 400)
         self.setWindowTitle("PyPomodoro - v1")
         self.setWindowIcon(QIcon("images/tomato.png"))
@@ -35,7 +37,7 @@ class PomodoroTimer(QWidget):
         self.short_break_limit = SHORT_BREAK_TIME
         self.long_break_limit = LONG_BREAK_TIME
 
-        self.setupTabsAndWidgets()
+        self.setup_tabs_and_widgets()
 
         self.current_tab_selected = 0
         self.current_start_button = self.pomodoro_start_button
@@ -45,11 +47,11 @@ class PomodoroTimer(QWidget):
         self.current_lcd = self.pomodoro_lcd
 
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.updateTimer)
+        self.timer.timeout.connect(self.update_timer)
 
         self.show()
 
-    def setupTabsAndWidgets(self):
+    def setup_tabs_and_widgets(self):
         self.tab_bar = QTabWidget(self)
         self.pomodoro_tab = QWidget()
         self.pomodoro_tab.setObjectName("Pomodoro")
@@ -62,19 +64,18 @@ class PomodoroTimer(QWidget):
         self.tab_bar.addTab(self.short_break_tab, "Short Break")
         self.tab_bar.addTab(self.long_break_tab, "Long Break")
 
-        self.tab_bar.currentChanged.connect(self.tabSwitched)
+        self.tab_bar.currentChanged.connect(self.tab_switched)
 
-        self.pomodoroTab()
-        self.shortBreakTab()
-        self.longBreakTab()
-
+        self.setup_pomodoro_tab()
+        self.setup_short_break_tab()
+        self.setup_long_break_tab()
 
         main_v_box = QVBoxLayout()
         main_v_box.addWidget(self.tab_bar)
         self.setLayout(main_v_box)
 
-    def pomodoroTab(self):
-        start_time = self.calculateDisplayTime(self.pomodoro_limit)
+    def setup_pomodoro_tab(self):
+        start_time = self.calculate_display_time(self.pomodoro_limit)
 
         self.pomodoro_lcd = QLCDNumber()
         self.pomodoro_lcd.setObjectName("PomodoroLCD")
@@ -82,13 +83,13 @@ class PomodoroTimer(QWidget):
         self.pomodoro_lcd.display(start_time)
 
         self.pomodoro_start_button = QPushButton("Start")
-        self.pomodoro_start_button.clicked.connect(self.startCountDown)
+        self.pomodoro_start_button.clicked.connect(self.start_countdown)
 
         self.pomodoro_stop_button = QPushButton("Stop")
-        self.pomodoro_stop_button.clicked.connect(self.stopCountDown)
+        self.pomodoro_stop_button.clicked.connect(self.stop_countdown)
 
         self.pomodoro_reset_button = QPushButton("Reset")
-        self.pomodoro_reset_button.clicked.connect(self.resetCountDown)
+        self.pomodoro_reset_button.clicked.connect(self.reset_countdown)
 
         button_h_box = QHBoxLayout()
         button_h_box.addWidget(self.pomodoro_start_button)
@@ -100,8 +101,8 @@ class PomodoroTimer(QWidget):
         v_box.addLayout(button_h_box)
         self.pomodoro_tab.setLayout(v_box)
 
-    def shortBreakTab(self):
-        start_time = self.calculateDisplayTime(self.short_break_limit)
+    def setup_short_break_tab(self):
+        start_time = self.calculate_display_time(self.short_break_limit)
 
         self.short_break_lcd = QLCDNumber()
         self.short_break_lcd.setObjectName("ShortLCD")
@@ -109,13 +110,13 @@ class PomodoroTimer(QWidget):
         self.short_break_lcd.display(start_time)
 
         self.short_start_button = QPushButton("Start")
-        self.short_start_button.clicked.connect(self.startCountDown)
+        self.short_start_button.clicked.connect(self.start_countdown)
 
         self.short_stop_button = QPushButton("Stop")
-        self.short_stop_button.clicked.connect(self.stopCountDown)
+        self.short_stop_button.clicked.connect(self.stop_countdown)
 
         self.short_reset_button = QPushButton("Reset")
-        self.short_reset_button.clicked.connect(self.resetCountDown)
+        self.short_reset_button.clicked.connect(self.reset_countdown)
 
         button_h_box = QHBoxLayout()
         button_h_box.addWidget(self.short_start_button)
@@ -127,8 +128,8 @@ class PomodoroTimer(QWidget):
         v_box.addLayout(button_h_box)
         self.short_break_tab.setLayout(v_box)
 
-    def longBreakTab(self):
-        start_time = self.calculateDisplayTime(self.long_break_limit)
+    def setup_long_break_tab(self):
+        start_time = self.calculate_display_time(self.long_break_limit)
 
         self.long_break_lcd = QLCDNumber()
         self.long_break_lcd.setObjectName("LongLCD")
@@ -136,13 +137,13 @@ class PomodoroTimer(QWidget):
         self.long_break_lcd.display(start_time)
 
         self.long_start_button = QPushButton("Start")
-        self.long_start_button.clicked.connect(self.startCountDown)
+        self.long_start_button.clicked.connect(self.start_countdown)
 
         self.long_stop_button = QPushButton("Stop")
-        self.long_stop_button.clicked.connect(self.stopCountDown)
+        self.long_stop_button.clicked.connect(self.stop_countdown)
 
         self.long_reset_button = QPushButton("Reset")
-        self.long_reset_button.clicked.connect(self.resetCountDown)
+        self.long_reset_button.clicked.connect(self.reset_countdown)
 
         button_h_box = QHBoxLayout()
         button_h_box.addWidget(self.long_start_button)
@@ -154,44 +155,45 @@ class PomodoroTimer(QWidget):
         v_box.addLayout(button_h_box)
         self.long_break_tab.setLayout(v_box)
 
-    def startCountDown(self):
+    def start_countdown(self):
         self.current_start_button.setEnabled(False)
 
-        remaining_time = self.calculateDisplayTime(self.current_time_limit)
+        remaining_time = self.calculate_display_time(self.current_time_limit)
 
         if remaining_time == "00:00":
-            self.resetCountDown()
+            self.reset_countdown()
             self.timer.start(1000)
         else:
             self.timer.start(1000)
 
-    def stopCountDown(self):
+    def stop_countdown(self):
         if self.timer.isActive():
             self.timer.stop()
             self.current_start_button.setEnabled(True)
-            self.stop_ticking_sound()
+        self.stop_all_sounds()
 
-    def resetCountDown(self):
-        self.stopCountDown()
+    def reset_countdown(self):
+        self.stop_countdown()
 
         if self.current_tab_selected == 0:
             self.pomodoro_limit = POMODORO_TIME
             self.current_time_limit = self.pomodoro_limit
-            reset_time = self.calculateDisplayTime(self.current_time_limit)
+            reset_time = self.calculate_display_time(self.current_time_limit)
         elif self.current_tab_selected == 1:
             self.short_break_limit = SHORT_BREAK_TIME
             self.current_time_limit = self.short_break_limit
-            reset_time = self.calculateDisplayTime(self.current_time_limit)
+            reset_time = self.calculate_display_time(self.current_time_limit)
         elif self.current_tab_selected == 2:
             self.long_break_limit = LONG_BREAK_TIME
             self.current_time_limit = self.long_break_limit
-            reset_time = self.calculateDisplayTime(self.current_time_limit)
+            reset_time = self.calculate_display_time(self.current_time_limit)
         self.current_lcd.display(reset_time)
 
-    def updateTimer(self):
-        remaining_time = self.calculateDisplayTime(self.current_time_limit)
+    def update_timer(self):
+        remaining_time = self.calculate_display_time(self.current_time_limit)
         if remaining_time == "00:00":
-            self.stopCountDown()
+            self.stop_countdown()
+            self.sound_alarm()
             self.current_lcd.display(remaining_time)
 
         else:
@@ -199,26 +201,31 @@ class PomodoroTimer(QWidget):
             self.current_lcd.display(remaining_time)
             self.play_ticking_sound()
 
+    def sound_alarm(self):
+        self.alarm_sound.stop()
+        self.alarm_sound.play()
+
     def play_ticking_sound(self):
         if self.current_tab_selected == 0:
             self.pomodoro_sound.stop()
             self.pomodoro_sound.play()
         elif self.current_tab_selected == 1:
-            self.sbreak_sound.stop()
-            self.sbreak_sound.play()
+            self.short_break_sound.stop()
+            self.short_break_sound.play()
         elif self.current_tab_selected == 2:
-            self.lbreak_sound.stop()
-            self.lbreak_sound.play()
+            self.long_break_sound.stop()
+            self.long_break_sound.play()
 
-    def stop_ticking_sound(self):
+    def stop_all_sounds(self):
         self.pomodoro_sound.stop()
-        self.sbreak_sound.stop()
-        self.lbreak_sound.stop()
+        self.short_break_sound.stop()
+        self.long_break_sound.stop()
+        self.alarm_sound.stop()
     
-    def tabSwitched(self, index):
+    def tab_switched(self, index):
         self.current_tab_selected = index
-        self.stopCountDown()
-        self.stop_ticking_sound()
+        self.stop_countdown()
+        self.stop_all_sounds()
 
         if self.current_tab_selected == 0:
             self.current_start_button = self.pomodoro_start_button
@@ -226,38 +233,41 @@ class PomodoroTimer(QWidget):
             self.current_reset_button = self.pomodoro_reset_button
             self.pomodoro_limit = POMODORO_TIME
             self.current_time_limit = self.pomodoro_limit
-
-            reset_time = self.calculateDisplayTime(self.current_time_limit)
+            reset_time = PomodoroTimer.calculate_display_time(self.current_time_limit)
             self.current_lcd = self.pomodoro_lcd
             self.current_lcd.display(reset_time)
+
         elif self.current_tab_selected == 1:
             self.current_start_button = self.short_start_button
             self.current_stop_button = self.short_stop_button
             self.current_reset_button = self.short_reset_button
             self.short_break_limit = SHORT_BREAK_TIME
             self.current_time_limit = self.short_break_limit
-
-            reset_time = self.calculateDisplayTime(self.current_time_limit)
+            reset_time = PomodoroTimer.calculate_display_time(self.current_time_limit)
             self.current_lcd = self.short_break_lcd
             self.current_lcd.display(reset_time)
+
         elif self.current_tab_selected == 2:
             self.current_start_button = self.long_start_button
             self.current_stop_button = self.long_stop_button
             self.current_reset_button = self.long_reset_button
             self.long_break_limit = LONG_BREAK_TIME
             self.current_time_limit = self.long_break_limit
-
-            reset_time = self.calculateDisplayTime(self.current_time_limit)
+            reset_time = PomodoroTimer.calculate_display_time(self.current_time_limit)
             self.current_lcd = self.long_break_lcd
             self.current_lcd.display(reset_time)
 
-    def convertTotalTime(self, time_in_milli):
-        minutes = (time_in_milli / (1000 * 60)) % 60
-        seconds = (time_in_milli / 1000) % 60
-        return int(minutes), int(seconds)
+    @staticmethod
+    def convert_total_time(time_in_milli):
+        hours = (time_in_milli // (1000 * 60)) // 60
+        minutes = (time_in_milli // (1000 * 60)) % 60
+        seconds = (time_in_milli // 1000) % 60
+        return hours, minutes, seconds
 
-    def calculateDisplayTime(self, time):
-        minutes, seconds = self.convertTotalTime(time)
+    @staticmethod
+    def calculate_display_time(time):
+        hours, minutes, seconds = PomodoroTimer.convert_total_time(time)
+        minutes += hours*60
         amount_of_time = "{:02d}:{:02d}".format(minutes, seconds)
         return amount_of_time
 
